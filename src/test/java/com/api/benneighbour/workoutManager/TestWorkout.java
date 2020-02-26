9d741893813e27b3972276621438f81b940e6f85
BenNeighbour <benneighbour007@gmail.com>
2020-02-26 20:14:31 +0000

    Creating test classes

--- src/test/java/com/api/benneighbour/workoutManager/TestWorkout.java
+++
@@ -1,5 +0,0 @@
-package com.api.benneighbour.workoutManager;
-
-public class TestWorkout {
-
-}
---
+++ src/test/java/com/api/benneighbour/workoutManager/UserTests.java
@@ -0,0 +1,32 @@
+package com.api.benneighbour.workoutManager;
+
+import com.api.benneighbour.workoutManager.user.dao.UserDao;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+public class UserTests {
+
+    @Mock
+    private UserDao dao;
+
+    @Test
+    public void findByNameTest() {
+    }
+
+    @Test
+    public void saveUserTest() {
+    }
+
+    @Test
+    public void updateUserTest() {
+    }
+
+    @Test
+    public void deleteUserTest() {
+    }
+
+    @Test
+    public void getAllUsersTest() {
+    }
+
+}
